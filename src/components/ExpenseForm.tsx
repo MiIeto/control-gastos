7267bf5ded12d9ea9f3e47d7e11bef33cdd93c39
Mileto <milenkotomasevic41@gmail.com>
2024-07-31 01:08:29 -0400

    dev: Added error message on form

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,23 +1,26 @@
-import { ChangeEvent, useState } from "react";
-import type { DraftExpense, Value } from "../types";
-import { categories } from "../data/categories";
-import DatePicker from "react-date-picker";
-import "react-calendar/dist/Calendar.css";
-import "react-date-picker/dist/DatePicker.css";
+import { ChangeEvent, FormEvent, useState } from 'react';
+import type { DraftExpense, Value } from '../types';
+import { categories } from '../data/categories';
+import DatePicker from 'react-date-picker';
+import 'react-calendar/dist/Calendar.css';
+import 'react-date-picker/dist/DatePicker.css';
+import ErrorMessage from './ErrorMessage';
 
 export default function ExpenseForm() {
 	const [expense, setExpense] = useState<DraftExpense>({
 		amount: 0,
-		expenseName: "",
-		category: "",
+		expenseName: '',
+		category: '',
 		date: new Date(),
 	});
+
+	const [error, setError] = useState('');
 
 	const handleChange = (
 		e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
 	) => {
 		const { name, value } = e.target;
-		const isAmountField = ["amount"].includes(name);
+		const isAmountField = ['amount'].includes(name);
 
 		setExpense({
 			...expense,
@@ -29,11 +32,25 @@ export default function ExpenseForm() {
 		setExpense({ ...expense, date: value });
 	};
 
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		//Validacion
+		if (Object.values(expense).includes('')) {
+			setError('Todos los campos son obligatorios');
+			return;
+		}
+
+        
+	};
+
 	return (
-		<form className="space-y-5">
+		<form className="space-y-5" onSubmit={handleSubmit}>
 			<legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
 				Nuevo Gasto
 			</legend>
+
+			{error && <ErrorMessage>{error}</ErrorMessage>}
 
 			<div className="flex flex-col gap-2">
 				<label htmlFor="expenseName" className="text-xl">
