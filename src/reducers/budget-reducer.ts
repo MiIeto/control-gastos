a30b1ea89a552d8d7ca1c4a570b38926cb1adb51
Mileto <milenkotomasevic41@gmail.com>
2024-07-30 23:41:34 -0400

    dev: Added actions to open and close modal

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,21 +1,36 @@
-export type BudgetActions = { type: "add-budget"; payload: { budget: number } };
+export type BudgetActions =
+	| { type: "add-budget"; payload: { budget: number } }
+	| { type: "show-modal"; }
+    | { type: "close-modal"; };
 
 export type BudgetState = {
 	budget: number;
+    modal: boolean;
 };
 
 export const initialState: BudgetState = {
 	budget: 0,
+    modal: false,
 };
 
 export const budgetReducer = (state: BudgetState, action: BudgetActions) => {
-    switch (action.type) {
-        case "add-budget":
+	switch (action.type) {
+		case "add-budget":
+			return {
+				...state,
+				budget: action.payload.budget,
+			};
+        case "show-modal":
             return {
                 ...state,
-                budget: action.payload.budget,
-            };
-        default:
-            return state;
-    }
+                modal: true,
+            }
+        case "close-modal":
+            return {
+                ...state,
+                modal: false,
+            }
+		default:
+			return state;
+	}
 };
