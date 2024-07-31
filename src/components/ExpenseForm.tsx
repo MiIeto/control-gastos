7605254e2b094221c446974b18d213faa1aa629a
Mileto <milenkotomasevic41@gmail.com>
2024-07-31 00:46:28 -0400

    dev: Added date and usestate to form

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+import type { DraftExpense } from "../types";
 import { categories } from "../data/categories";
+import DatePicker from "react-date-picker";
+import "react-calendar/dist/Calendar.css";
+import "react-date-picker/dist/DatePicker.css";
 
 export default function ExpenseForm() {
+	const [expense, setExpense] = useState<DraftExpense>({
+		amount: 0,
+		expenseName: "",
+		category: "",
+		date: new Date(),
+	});
+
 	return (
 		<form className="space-y-5">
 			<legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
@@ -17,6 +29,7 @@ export default function ExpenseForm() {
 					placeholder="Añade el nombre del gasto"
 					className="bg-slate-100 p-2"
 					name="expenseName"
+					value={expense.expenseName}
 				/>
 			</div>
 			<div className="flex flex-col gap-2">
@@ -29,6 +42,7 @@ export default function ExpenseForm() {
 					placeholder="Añade la cantidad del gasto: ej. 300"
 					className="bg-slate-100 p-2"
 					name="amount"
+					value={expense.amount}
 				/>
 			</div>
 
@@ -40,6 +54,7 @@ export default function ExpenseForm() {
 					name="category"
 					id="category"
 					className="bg-slate-100 p-2"
+					value={expense.category}
 				>
 					<option value="">-- Seleccione --</option>
 					{categories.map((category) => (
@@ -48,6 +63,16 @@ export default function ExpenseForm() {
 						</option>
 					))}
 				</select>
+			</div>
+
+			<div className="flex flex-col gap-2">
+				<label htmlFor="amount" className="text-xl">
+					Fecha Gasto:
+				</label>
+				<DatePicker
+					className="bg-slate-100 p-2 border-0"
+					value={expense.date}
+				/>
 			</div>
 
 			<input
