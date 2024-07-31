0d0c42f2a30b6ad7e0a41829c53f50930f903438
Mileto <milenkotomasevic41@gmail.com>
2024-07-31 01:19:13 -0400

    dev: Added function to add expense

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -5,6 +5,7 @@ import DatePicker from 'react-date-picker';
 import 'react-calendar/dist/Calendar.css';
 import 'react-date-picker/dist/DatePicker.css';
 import ErrorMessage from './ErrorMessage';
+import { useBudget } from '../hooks/useBudget';
 
 export default function ExpenseForm() {
 	const [expense, setExpense] = useState<DraftExpense>({
@@ -15,6 +16,7 @@ export default function ExpenseForm() {
 	});
 
 	const [error, setError] = useState('');
+	const { dispatch } = useBudget();
 
 	const handleChange = (
 		e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
@@ -41,81 +43,100 @@ export default function ExpenseForm() {
 			return;
 		}
 
-        
+		// Agregar un nuevo gasto
+		dispatch({ type: 'add-expense', payload: { expense } });
 	};
 
 	return (
-		<form className="space-y-5" onSubmit={handleSubmit}>
-			<legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
+		<form
+			className='space-y-5'
+			onSubmit={handleSubmit}
+		>
+			<legend className='uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2'>
 				Nuevo Gasto
 			</legend>
 
 			{error && <ErrorMessage>{error}</ErrorMessage>}
 
-			<div className="flex flex-col gap-2">
-				<label htmlFor="expenseName" className="text-xl">
+			<div className='flex flex-col gap-2'>
+				<label
+					htmlFor='expenseName'
+					className='text-xl'
+				>
 					Nombre Gasto:
 				</label>
 				<input
-					type="text"
-					id="expenseName"
-					placeholder="Añade el nombre del gasto"
-					className="bg-slate-100 p-2"
-					name="expenseName"
+					type='text'
+					id='expenseName'
+					placeholder='Añade el nombre del gasto'
+					className='bg-slate-100 p-2'
+					name='expenseName'
 					value={expense.expenseName}
 					onChange={handleChange}
 				/>
 			</div>
-			<div className="flex flex-col gap-2">
-				<label htmlFor="amount" className="text-xl">
+			<div className='flex flex-col gap-2'>
+				<label
+					htmlFor='amount'
+					className='text-xl'
+				>
 					Cantidad:
 				</label>
 				<input
-					type="number"
-					id="amount"
-					placeholder="Añade la cantidad del gasto: ej. 300"
-					className="bg-slate-100 p-2"
-					name="amount"
+					type='number'
+					id='amount'
+					placeholder='Añade la cantidad del gasto: ej. 300'
+					className='bg-slate-100 p-2'
+					name='amount'
 					value={expense.amount}
 					onChange={handleChange}
 				/>
 			</div>
 
-			<div className="flex flex-col gap-2">
-				<label htmlFor="amount" className="text-xl">
+			<div className='flex flex-col gap-2'>
+				<label
+					htmlFor='amount'
+					className='text-xl'
+				>
 					Categoria:
 				</label>
 				<select
-					name="category"
-					id="category"
-					className="bg-slate-100 p-2"
+					name='category'
+					id='category'
+					className='bg-slate-100 p-2'
 					value={expense.category}
 					onChange={handleChange}
 				>
-					<option value="">-- Seleccione --</option>
+					<option value=''>-- Seleccione --</option>
 					{categories.map((category) => (
-						<option key={category.id} value={category.id}>
+						<option
+							key={category.id}
+							value={category.id}
+						>
 							{category.name}
 						</option>
 					))}
 				</select>
 			</div>
 
-			<div className="flex flex-col gap-2">
-				<label htmlFor="amount" className="text-xl">
+			<div className='flex flex-col gap-2'>
+				<label
+					htmlFor='amount'
+					className='text-xl'
+				>
 					Fecha Gasto:
 				</label>
 				<DatePicker
-					className="bg-slate-100 p-2 border-0"
+					className='bg-slate-100 p-2 border-0'
 					value={expense.date}
 					onChange={handleChangeDate}
 				/>
 			</div>
 
 			<input
-				type="submit"
-				className="bg-blue-500 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
-				value="Registrar Gasto"
+				type='submit'
+				className='bg-blue-500 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg'
+				value='Registrar Gasto'
 			/>
 		</form>
 	);
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,35 +1,56 @@
+import { v4 as uuidv4 } from 'uuid';
+import { DraftExpense, Expense } from '../types';
+
 export type BudgetActions =
-	| { type: "add-budget"; payload: { budget: number } }
-	| { type: "show-modal"; }
-    | { type: "close-modal"; };
+	| { type: 'add-budget'; payload: { budget: number } }
+	| { type: 'show-modal' }
+	| { type: 'close-modal' }
+	| { type: 'add-expense'; payload: { expense: DraftExpense } };
 
 export type BudgetState = {
 	budget: number;
-    modal: boolean;
+	modal: boolean;
+	expenses: Expense[];
 };
 
 export const initialState: BudgetState = {
 	budget: 0,
-    modal: false,
+	modal: false,
+	expenses: [],
+};
+
+const createExpense = (draftExpense: DraftExpense): Expense => {
+	return {
+		...draftExpense,
+		id: uuidv4(),
+	};
 };
 
 export const budgetReducer = (state: BudgetState, action: BudgetActions) => {
 	switch (action.type) {
-		case "add-budget":
+		case 'add-budget':
 			return {
 				...state,
 				budget: action.payload.budget,
 			};
-        case "show-modal":
-            return {
-                ...state,
-                modal: true,
-            }
-        case "close-modal":
-            return {
-                ...state,
-                modal: false,
-            }
+		case 'show-modal':
+			return {
+				...state,
+				modal: true,
+			};
+		case 'close-modal':
+			return {
+				...state,
+				modal: false,
+			};
+		case 'add-expense': {
+			const expense = createExpense(action.payload.expense);
+			return {
+				...state,
+				expenses: [...state.expenses, expense],
+			};
+		}
+
 		default:
 			return state;
 	}
