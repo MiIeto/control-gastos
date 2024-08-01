89b63729218a138bf0c8c1ed76c65d2ea484431a
Mileto <milenkotomasevic41@gmail.com>
2024-07-31 20:38:27 -0400

    dev: Added dinamic amounts

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,32 +1,47 @@
-import AmountDisplay from "./AmountDisplay";
+import { useMemo } from 'react';
+import { useBudget } from '../hooks/useBudget';
+import AmountDisplay from './AmountDisplay';
 
 export default function BudgetTracker() {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        <div className="flex justify-center">
-            <img src="/grafico.jpg" alt="Grafico de gastos" />
-        </div>
-        <div className="flex flex-col justify-center items-center gap-8">
-            <button
-                type="button"
-                className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
-            >
-                Resetear App
-            </button>
+	const { state } = useBudget();
+	const totalExpenses = useMemo(
+		() =>
+			state.expenses.reduce(
+				(total, expense) => total + expense.amount,
+				0
+			),
+		[state.expenses]
+	);
+    const remainingBudget = state.budget - totalExpenses;
+	return (
+		<div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+			<div className='flex justify-center'>
+				<img
+					src='/grafico.jpg'
+					alt='Grafico de gastos'
+				/>
+			</div>
+			<div className='flex flex-col justify-center items-center gap-8'>
+				<button
+					type='button'
+					className='bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg'
+				>
+					Resetear App
+				</button>
 
-            <AmountDisplay 
-                label="Presupuesto"
-                amount={300}
-            />
-            <AmountDisplay 
-                label="Disponible"
-                amount={200}
-            />
-            <AmountDisplay 
-                label="Gastado"
-                amount={100}
-            />
-        </div>
-    </div>
-  )
+				<AmountDisplay
+					label='Presupuesto'
+					amount={state.budget}
+				/>
+				<AmountDisplay
+					label='Disponible'
+					amount={remainingBudget}
+				/>
+				<AmountDisplay
+					label='Gastado'
+					amount={totalExpenses}
+				/>
+			</div>
+		</div>
+	);
 }
