fce7bc725b8b0fe3233eacd354779804b35f295b
Mileto <milenkotomasevic41@gmail.com>
2024-07-31 20:09:13 -0400

    dev: Added action to get id of expense

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -6,18 +6,21 @@ export type BudgetActions =
 	| { type: 'show-modal' }
 	| { type: 'close-modal' }
 	| { type: 'add-expense'; payload: { expense: DraftExpense } }
-	| { type: 'remove-expense'; payload: { id: Expense['id'] } };
+	| { type: 'remove-expense'; payload: { id: Expense['id'] } }
+	| { type: 'get-expense-by-id'; payload: { id: Expense['id'] } };
 
 export type BudgetState = {
 	budget: number;
 	modal: boolean;
 	expenses: Expense[];
+	editingId: Expense['id'];
 };
 
 export const initialState: BudgetState = {
 	budget: 0,
 	modal: false,
 	expenses: [],
+	editingId: '',
 };
 
 const createExpense = (draftExpense: DraftExpense): Expense => {
@@ -59,6 +62,13 @@ export const budgetReducer = (state: BudgetState, action: BudgetActions) => {
 					(expense) => expense.id !== action.payload.id
 				),
 			};
+		}
+		case 'get-expense-by-id': {
+			return{
+				...state,
+				editingId: action.payload.id,
+				modal: true,
+			}
 		}
 		default:
 			return state;
