32a5f4423c5cf7592526d3fef9e6709868dc56a4
Mileto <milenkotomasevic41@gmail.com>
2024-07-31 21:27:11 -0400

    dev: Updated name to reset-app

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -4,29 +4,29 @@ import AmountDisplay from './AmountDisplay';
 import 'react-circular-progressbar/dist/styles.css';
 
 export default function BudgetTracker() {
+	const { state, dispatch, totalExpenses, remainingBudget } = useBudget();
 
-    const { state, totalExpenses, remainingBudget } = useBudget();
+	const percentage = +((totalExpenses / state.budget) * 100).toFixed(2);
 
-    const percentage = +((totalExpenses / state.budget) * 100).toFixed(2);
-	
 	return (
 		<div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
 			<div className='flex justify-center'>
 				<CircularProgressbar
-                    value={percentage}
-                    styles={buildStyles({
-                        pathColor: percentage === 100 ? '#DC2626' : '#3b82f6',
-                        trailColor: '#f5f5f5',
-                        textSize: 8,
-                        textColor: percentage === 100 ? '#DC2626' : '#3b82f6',
-                    })}
-                    text={`${percentage}% Gastado`}
-                />
+					value={percentage}
+					styles={buildStyles({
+						pathColor: percentage === 100 ? '#DC2626' : '#3b82f6',
+						trailColor: '#f5f5f5',
+						textSize: 8,
+						textColor: percentage === 100 ? '#DC2626' : '#3b82f6',
+					})}
+					text={`${percentage}% Gastado`}
+				/>
 			</div>
 			<div className='flex flex-col justify-center items-center gap-8'>
 				<button
 					type='button'
 					className='bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg'
+					onClick={() => dispatch({ type: 'reset-app' })}
 				>
 					Resetear App
 				</button>
