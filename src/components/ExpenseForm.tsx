fbc31f1a37dcdf9470c88ea891d721543b643569
Mileto <milenkotomasevic41@gmail.com>
2024-07-31 20:27:05 -0400

    dev: Updated conditional messages on form

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -77,7 +77,7 @@ export default function ExpenseForm() {
 			onSubmit={handleSubmit}
 		>
 			<legend className='uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2'>
-				Nuevo Gasto
+				{state.editingId ? 'Guardar Cambios' : 'Nuevo Gasto'}
 			</legend>
 
 			{error && <ErrorMessage>{error}</ErrorMessage>}
@@ -160,7 +160,7 @@ export default function ExpenseForm() {
 			<input
 				type='submit'
 				className='bg-blue-500 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg'
-				value='Registrar Gasto'
+				value={state.editingId ? 'Guardar Cambios' : 'Registrar Gasto'}
 			/>
 		</form>
 	);
